2e3f9edd9ec952a66db7788629a91b9b97a7a2a4
duxuhao <duxuhao88@gmail.com>
2016-12-03 21:35:18 +0800

    add the minifold for reducing dimension

--- CountBubble.py
+++ CountBubble.py
@@ -1,6 +1,9 @@
 import wavio
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn import manifold
+import pandas as pd
+import xgboost as xgb
 
 
 class CountBubble():
@@ -29,13 +32,36 @@ class CountBubble():
         plt.plot(self.ThresholdMethod(threshold) * 100000, 'r')
         plt.plot(self.smoothdata[1:],'k')
         plt.show()
+    
+    def ManifoldTrain(self, target):
+        train = pd.read_csv('train.csv')
+        self.tf = manifold.LocallyLinearEmbedding(n_neighbors, n_components,eigen_solver='auto')
+        x = self.tf.fit_transform(train.ix[:,:-3])
+        if len(np.unique(train[target])) < 3:
+            self.clf = xgb.XGBClassifier()
+        else:
+            self.clf = xgb.XGBRegressor()
+        self.clf.fit(x, train[target])
+    
+    def ManifoldNum(self):
+        df = self.data.reshape(100,-1)
+        x = self.tf.transform(df)
+        prediction = self.clf.predict(x)
+        return sum(prediction)
+    
+    def ManifoldWidth(self):
+        df = self.data.reshape(100,-1)
+        x = self.tf.transform(df)
+        prediction = self.clf.predict(x)
+        return prediction
 
 if __name__ == "__main__":
     filename = 'B18h01m41s17jul2014y.wav'
     threshold = 8000
     StartTime = 3.1
-    #EndTime = 3.3
-    for EndTime in np.arange(StartTime + 0.1,3.3,0.1):
-        sample = CountBubble(filename, StartTime, EndTime)
-        sample.ThresholdMethod(threshold)
-        sample.draw(threshold)
+    EndTime = 3.3
+    sample = CountBubble(filename, StartTime, EndTime)
+    #sample.ThresholdMethod(threshold)
+    #sample.draw(threshold)
+    sample.ManifoldTrain(Num)
+    print sample.ManifoldNum()
