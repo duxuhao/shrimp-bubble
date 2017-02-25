de17dbce08848589a9fc265f99e6ad5a0eab6746
root <root@ga2.novalocal>
2017-02-25 13:52:23 +1100

    add normalized and double trained

--- ShrimpAudioPredict.py
+++ ShrimpAudioPredict.py
@@ -153,9 +153,9 @@ manifoldWPEnergyModel = TrainManifoldSnappingShrimp.ManifoldTrain(TrainManifoldS
 clf, fealist = TrainClaasifier(ClssifySnappingShrimp, labelfile, manifoldWPEnergyModel, manifoldWPFlatnessModel, clf)
 
 dir = 'X'
-predictionfilelist = ['B17h23m35s25apr2012y.wav','B09h39m21s17jul2011y.wav','B17h12m11s09jul2009y.wav','B18h17m19s19jan2009y.wav','B18h39m48s26apr2012y.wav','B18h01m41s17jul2014y.wav','B12h31m11s04oct2007y.wav','B11h08m25s24aug2007y.wav','B12h35m21s29apr2008y.wav','B16h03m56s10sep2007y.wav','B12h08m04s29apr2008y.wav']
+predictionfilelist = ['081218102553.wav','081218184245.wav','081219050350.wav']#['B17h23m35s25apr2012y.wav','B09h39m21s17jul2011y.wav','B17h12m11s09jul2009y.wav','B18h17m19s19jan2009y.wav','B18h39m48s26apr2012y.wav','B18h01m41s17jul2014y.wav','B12h31m11s04oct2007y.wav','B11h08m25s24aug2007y.wav','B12h35m21s29apr2008y.wav','B16h03m56s10sep2007y.wav','B12h08m04s29apr2008y.wav']
 for predictfile in predictionfilelist[:]:
-    logfilename = predictfile[:-4]+'.csv'
+    logfilename = predictfile[:-4]+'all.csv'
     PreStartTime = 0.0
     PreEndTime = 60*100.0
     try:
